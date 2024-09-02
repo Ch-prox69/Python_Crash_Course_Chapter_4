f6ccf7e11f6b244b210c69925bac339945ee390e
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-02 11:24:13 -0400

    Add files via upload

---
+++ hartzell_TIY_4.2.py
@@ -0,0 +1,11 @@
+# The animals list 
+animls_list = ['Cat', 'Dog', 'Tiger']
+for animls in animls_list:
+    print(animls)
+
+# A stament for each animal
+    print(f"\nA {animls.title()}, is a good pick for an animal")
+
+
+# This is the end of line statment
+print("\nAll of these animals are a good choice for a pet")
