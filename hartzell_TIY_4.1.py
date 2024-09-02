06f1ebf631e4d2bf18d3a9ef6e6b0fa7cb0a82ea
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-02 11:05:14 -0400

    Add files via upload

---
+++ hartzell_TIY_4.1.py
@@ -0,0 +1,10 @@
+# A Pizza Party List
+pizza_list = ['Sausage Pizza', 'Pepperoni Pizza', 'Taco Pizza']
+
+for pizza in pizza_list:
+    print(pizza)
+
+# Now lets modify the loop to print a message about the pizza
+    print(f"I like {pizza.title()}.")
+
+print("Man I love pizza. I like Sausage Pizza. I like Pepperoni Pizza. I like Taco Pizza. Man, I love pizza. Thank you Italy for delicous pizza")
