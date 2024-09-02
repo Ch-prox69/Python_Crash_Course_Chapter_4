065bbb3ec6233a75dc96338a07b6e3dfea627823
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-02 15:36:18 -0400

    Add files via upload

---
+++ hartzell_TIY_4.5.py
@@ -0,0 +1,11 @@
+# Revised Onemillion list
+digits = list(range(1, 1000001))
+
+# The min and max
+print("The minimum digits are...",min(digits))
+print("The maximum dgits are...",max(digits))
+
+# The total sum bruahh
+print("The total sum of digits is... ",sum(digits))
+
+print("Wow, that's a lot of digits!")
