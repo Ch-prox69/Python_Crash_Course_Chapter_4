9a0fa765919b9aee2e4453e75bec00c58639e7bf
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-03 19:40:03 -0400

    Add files via upload

---
+++ hartzell_TIY_4.13.py
@@ -0,0 +1,11 @@
+buffet_food = ("Pizza", "Burgers", "Fish", "Hushpuppies", "Pudding")
+print("The Original Menu:")
+for buffet in buffet_food:
+    print(buffet)
+
+# Here i did dimensions[0] = bruh and it created a tuple error
+
+revisedBuffet_food = ("Pizza", "Burgers", "Fish", "Hot Dogs", "Oysters")
+print("The Modified List:")
+for revised in revisedBuffet_food:
+    print(revised)
