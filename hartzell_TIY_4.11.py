96113476f0f3b30928b80f0622c40250afe513dc
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-03 13:19:18 -0400

    Add files via upload

---
+++ hartzell_TIY_4.11.py
@@ -0,0 +1,16 @@
+pizza_list = ['Sausage Pizza', 'Pepperoni Pizza', 'Taco Pizza',]
+friend_pizzas = ['Sausage Pizza', 'Pepperoni Pizza', 'Taco Pizza',]
+
+# Adding pizzas to pizza_list
+pizza_list.append('Anchovies Pizza')
+
+# Adding pizzas to the friend_pizza list
+friend_pizzas.append('Pineapple Pizza')
+
+# Using print and for loop for my list
+for pizzas in pizza_list:
+    print("My favorite pizzas are:",pizzas)
+
+# Using print and for loop for friend pizza list
+for friends in friend_pizzas:
+    print("\nMy friend's favorite pizzas are:",friends)
