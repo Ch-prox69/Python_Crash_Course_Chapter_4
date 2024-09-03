a09f83bfa306df1a284960a9459efb8431bf324f
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-03 12:14:48 -0400

    Add files via upload

---
+++ hartzell_TIY_4.10.py
@@ -0,0 +1,21 @@
+# The animals list
+animls_list = ['Cat', 'Dog', 'Tiger', 'Monkey', 'Kangaroo', 'Sloth', 'Zebra', 'Donkey', 'Bear']
+for animls in animls_list:
+    print(animls)
+
+# A stament for each animal
+    print(f"\nA {animls.title()}, is a good pick for an animal")
+
+
+# This is the end of line statment
+print("\nAll of these animals are a good choice for a pet")
+
+# Slicing the list: First Three
+print("The first three items in the list are:",animls_list[0:3])
+
+# Slicing the list: Middle Three
+print("Three items from the middle list are:", animls_list[3:6])
+
+# Slicing the list: Last Three
+print("The last three items in the list are:", animls_list[-3:])
+
