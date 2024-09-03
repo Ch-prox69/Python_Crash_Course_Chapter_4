7546e8a4520d11d16c2136699482a6d6ed9fac7a
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-03 13:30:59 -0400

    Add files via upload

---
+++ hartzell_TIY_4.12.py
@@ -0,0 +1,10 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+second_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+
+# Time to print!!!
+for food in my_foods:
+    print("The first food list is...", food)
+
+# Time to print the second list!
+for second in second_foods:
+    print("\nThe second food list is...", second)
